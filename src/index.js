bc9b69cdcdbb772f2132038d1bede29d405a7c96
Fisherenok <34278394+fisherenok@users.noreply.github.com>
2021-02-02 21:06:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter><App /></HashRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
